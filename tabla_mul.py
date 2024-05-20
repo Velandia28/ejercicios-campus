9d25c322dd4ae22c8189b02dceb4534a87a41bfd
Velandia28 <luisfelipevll28@gmail.com>
2024-05-20 13:10:03 -0500

    tabla ejercicio

---
+++ tabla_mul.py
@@ -0,0 +1,7 @@
+#Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
+
+num =int(input("digite un numero para realzair una tabla de multiplicar " ))
+
+for tabla  in range (0,11,1):
+    print (num*tabla)
+
