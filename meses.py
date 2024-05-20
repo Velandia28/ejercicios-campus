32796c15adf2a89bd2a12bb430227080f6b5cea5
Velandia28 <luisfelipevll28@gmail.com>
2024-05-20 11:56:45 -0500

    ejercicio uno

---
+++ meses.py
@@ -0,0 +1,26 @@
+meses  =int( input("por favor digiete el numero de su mes" ))
+
+if meses ==1:
+    print =("mes digitado es enero")
+elif meses ==2:
+    print ("El mes digitado es febrero" )
+elif meses ==3:
+    print  ("el mes digitado es marzo")
+elif meses ==4:
+    print ("el mes digitado es abril" )
+elif meses ==5:
+    print ("el mes digitado es mayo")
+elif meses ==6:
+    print ("el mes digitado es junio" )
+elif meses ==7:
+    print ("el mes digitado es julio ")
+elif meses ==8:
+    print ("el mes digitado es agosto ")
+elif meses ==9:
+    print ("el mes digitado es septiembre")    
+elif meses ==10:
+    print ("el mes digitado es octubre" )
+elif meses ==11:
+    print ("el mes digitado es noviembre ")
+elif meses ==12:
+    print ("el mes digitado es  diciembre")
