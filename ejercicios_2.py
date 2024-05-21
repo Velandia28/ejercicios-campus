f1f5a61da395ded91c44e2de9dd0e369b4bb7b7c
Velandia28 <luisfelipevll28@gmail.com>
2024-05-21 17:56:24 -0500

    finalizado

--- ejercicios_2.py
+++ ejercicios_2.py
@@ -1,16 +1,16 @@
 # Escribir una función que calcule el área de un círculo y
 # otra que calcule el volumen de un cilindro usando la primera función.
 PI = 3.14
+area =(float(input("digite el radio el circulo : ")))
 def circulo1 (area):
     circulo =PI*(area**2)
-    return  circulo
-print("el area del circulo es ", circulo1(2))
+    return  circulo 
+print(circulo1(area)," es la area del circulo")
 
+altura=(float(input("digite la altura del cilindro : ")))
 def cilindro(altura):
-    volumen=(circulo1 *altura)
-    return volumen
-
-print("el volumen del cilindro es" ,cilindro)
-
+    volumen1 =(PI*(area**2)*altura)
+    return volumen1
+print("el volumen del cilindro es ", cilindro(altura))
 
  
