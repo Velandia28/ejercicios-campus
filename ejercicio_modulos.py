b4cc227cb949a4dc5d434eca2ae1f787f1fcc1d1
Velandia28 <luisfelipevll28@gmail.com>
2024-05-28 13:19:14 -0500

    pendiente

---
+++ ejercicio_modulos.py
@@ -0,0 +1,12 @@
+datos={
+    "inscritos"{
+    "131312": {"nombre":"luis", "edad": 21, "departamento": "santander"},
+    "12383":{"nombre":"laura", "edad": 23, "departamento": "santander"},
+    "452211": {"nombre":"jorge", "edad": 30, "departamento": "santander"}
+    },
+    "carrera"{
+        "atletimo":{"131312": {"nombre":"luis", "edad": 21, "departamento": "santander"}},
+        "ciclimo":{"12383":{"nombre":"laura", "edad": 23, "departamento": "santander"}},
+        "patinaje":{"452211": {"nombre":"jorge", "edad": 30, "departamento": "santander"}}
+        }
+    }
